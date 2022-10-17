4bc38267834eb8ad6f3aeaf7531766ba6b46e530
Chelovechina <zhkk2004@gmail.com>
2022-10-17 20:42:58 +0600

    feat(dream-team): implement dream-team subtask

--- src/dream-team.js
+++ src/dream-team.js
@@ -2,22 +2,52 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create name of dream team based on the names of its members
- *  
- * @param {Array} members names of the members 
+ *
+ * @param {Array} members names of the members
  * @return {String | Boolean} name of the team or false
  * in case of incorrect members
  *
  * @example
- * 
+ *
  * createDreamTeam(['Matt', 'Ann', 'Dmitry', 'Max']) => 'ADMM'
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function createDreamTeam(members) {
+  if (Array.isArray(members) && members.length > 0) {
+    let name = '';
+
+    members.forEach((el) => {
+      if (typeof el === 'string') {
+        el = el.split(' ').join('');
+        name += el[0];
+      }
+    });
+
+    name = name.toUpperCase();
+    name = name.split('').sort().join('');
+    return name;
+  }
+
+  return false;
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
+
+console.log(
+  createDreamTeam([
+    ['David Abram'],
+    ['Robin Attfield'],
+    'Thomas Berry',
+    ['Paul R.Ehrlich'],
+    'donna Haraway',
+    ' BrIaN_gOodWiN  ',
+    {
+      0: 'Serenella Iovino',
+    },
+    'Erazim Kohak',
+    '  val_plumwood',
+  ]),
+);
